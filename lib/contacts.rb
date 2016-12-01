696d1b5c9b070e133eb369b94b3193019312e535
Getro Naissanse and James Lee <james@>
2016-12-01 09:28:27 -0800

    add specs(4), they have all successfully passed

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,5 +1,10 @@
 class Contacts
-  define_method(:initialize) do ||
-    #code here
+  attr_reader(:first_name, :last_name, :job_title, :company)
+
+  define_method(:initialize) do |attributes|
+    @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    @job_title = attributes.fetch(:job_title)
+    @company = attributes.fetch(:company)
   end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -1,10 +1,29 @@
 require('rspec')
 require('contacts')
 
-describe("#Contacts") do
-  describe("") do
-    it("") do
-      expect().to(eq())
+describe(Contacts) do
+  describe("#first_name") do
+    it("will return the first name") do
+      test_contact = Contacts.new({:first_name => "Getro", :last_name =>"Naissance", :job_title =>"Engineer", :company => "Google"})
+      expect(test_contact.first_name()).to(eq("Getro"))
+    end
+  end
+  describe("#last_name") do
+    it("will return the last name") do
+      test_contact = Contacts.new({:first_name => "Getro", :last_name =>"Naissance", :job_title =>"Engineer", :company => "Google"})
+      expect(test_contact.last_name()).to(eq("Naissance"))
+    end
+  end
+  describe("#job_title") do
+    it("will return the job title") do
+      test_contact = Contacts.new({:first_name => "Getro", :last_name => "Naissance", :job_title =>"Engineer", :company => "Google"})
+      expect(test_contact.job_title()).to(eq("Engineer"))
+    end
+  end
+  describe("#company") do
+    it("will return the company name") do
+      test_contact = Contacts.new({:first_name => "Getro", :last_name => "Naissance", :job_title => "Engineer", :company => "Google"})
+      expect(test_contact.company()).to(eq("Google"))
     end
   end
 end
